4f98d139767ec5184bce3f7c20fa645fa226b058
Manuel Honoré de Sousa <71514733+manuelhonoredesousa@users.noreply.github.com>
2024-08-19 19:15:02 +0100

    👨‍💻before add ajust width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { Input } from "@/components/input";
 import { InputText } from "primereact/inputtext";
 
 import { Password } from "primereact/password";
+import { View } from "@/components/view";
 
 //TODO need to fix hydration error
 export default function Home() {
@@ -17,54 +18,45 @@ export default function Home() {
   // }
 
   return (
-    <main className="min-h-screen bg-akin-cosmic-latte">
-      <section className="grid grid-cols-3 *:border">
-        <aside>
+    <main className="min-h-svh bg-akin-cosmic-latte">
+      <section className="grid md:grid-cols-3  container m-auto ">
+        <aside className="space-y-24 p-8 md:col-span-1">
           <Image
             src={APP_CONFIG.LOGO}
             alt="Akin logo"
             width={150}
             height={150}
           />
-          <div>
-            <div>
-              <label>
-                <strong className="text-akin-turquoise">Email:</strong>
-                <InputText type="email" placeholder="Digite o seu e-amail" />
-              </label>
-            </div>
-            <div>
-              <label>
-                <strong className="text-akin-turquoise">Password:</strong>
-                <Password
-                  placeholder="Digite a sua password"
-                  feedback={false}
-                  tabIndex={1}
-                  toggleMask
-                />
-              </label>
-            </div>
-            <div className="flex justify-between items-center">
+          <View.Vertical className="space-y-2">
+            <View.Vertical>
+              <strong className="text-akin-turquoise">Email:</strong>
+              <InputText
+                type="email"
+                placeholder="Digite o seu e-amail"
+                className="border-2 border-akin-yellow-light p-3 rounded-lg bg-akin-yellow-light/20 ring-0"
+              />
+            </View.Vertical>
+            <View.Vertical>
+              <strong className="text-akin-turquoise">Password:</strong>
+              <InputText
+                type="password"
+                placeholder="Digite a sua password"
+                className="border-2 border-akin-yellow-light p-3 rounded-lg bg-akin-yellow-light/20 ring-0"
+              />
+            </View.Vertical>
+            <View.Horizontal className="justify-between items-center">
               <p>Relembrar-me</p>
               <Input.Switch />
-            </div>
-            <Button.Primary label="Entrar" />
-          </div>
+            </View.Horizontal>
+            <View.Horizontal className="justify-end pt-4">
+              <Button.Primary label="Entrar" />
+            </View.Horizontal>
+          </View.Vertical>
         </aside>
-        <aside className="col-span-2">
+        <aside className="col-span-2 hidden md:flex ">
           <Image src={ImageAkinDemo} alt="Akin Demo" />
         </aside>
       </section>
     </main>
   );
-}
-
-
-function VStack () {
-  return
-  
-}
-function HStack () {
-  return
-  
 }
