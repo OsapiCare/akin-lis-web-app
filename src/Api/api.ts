9100c83c7818f1ee715ece90234387a0a60558f0
adolfodev10 <adolfomanueldev10@gmail.com>
2025-12-12 19:45:52 +0100

    fix: display allocated chief name instead of logged-in user for exams

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://magnetic-buzzard-osapicare-a83d5229.koyeb.app",
+  // baseURL: "https://drab-pig-osapicare-790df34d.koyeb.app/",
 });
 
 // export const api = axios.create({
@@ -11,4 +12,5 @@ export const api = axios.create({
 
 export const agentUrl = axios.create({
   baseURL: "https://chatkin.osapicare.com",
+  // baseURL: "https://osapicare.pythonanywhere.com",
 })
